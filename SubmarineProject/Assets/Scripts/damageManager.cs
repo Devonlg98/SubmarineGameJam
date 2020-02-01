2122f15c9131e7da127b09af81af97be7a144f0d
Joseph Fisher <LGqNVp@x&H=P7Vbq>
2020-02-01 00:15:44 -0800

    Added Random Spawn Enable

---
+++ SubmarineProject/Assets/Scripts/damageManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageManager : MonoBehaviour
+{
+    public damageEnable[] type;
+
+    public void createDamage() 
+    {
+        int enabledType = Random.Range(0, 2);
+        type[enabledType].enableDamage();
+    }
+}
