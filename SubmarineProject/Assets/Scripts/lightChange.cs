09416c43630a49486f6bc90380b1a52120895d51
Joseph Fisher <LGqNVp@x&H=P7Vbq>
2020-02-01 18:07:05 -0800

    AddedLightChanges

--- SubmarineProject/Assets/Scripts/lightChange.cs
+++ SubmarineProject/Assets/Scripts/lightChange.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class lightChange : MonoBehaviour
 {
     public Light[] shipLights;
+    public Color baseColor;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) 
         {
-            changeLights();
+            changeLightsRed();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            changeLightsWhite();
         }
     }
-    void changeLights() 
+    void changeLightsRed() 
     {
         for (int i = 0; i < shipLights.Length; i++)
         {
             shipLights[i].color = Color.red;
+        }
+    }
+
+    void changeLightsWhite()
+    {
+        for (int i = 0; i < shipLights.Length; i++)
+        {
+            shipLights[i].color = baseColor;
         }
     }
 }
