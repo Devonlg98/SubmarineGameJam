cbe0caee724044fa6e67c8dbeb5bcb7c288b2fa7
SamuelDaman <54036673+SamuelDaman@users.noreply.github.com>
2020-02-01 18:09:49 -0800

    Sub Movement is complete now working on torpedos

---
+++ SubmarineProject/Assets/Scripts/Torpedo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torpedo : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * 5;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Torpedo explodes and destroys the any obstacles it hits.
+        Destroy(gameObject);
+    }
+}
